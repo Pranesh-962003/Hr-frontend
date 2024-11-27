7be9f705cf4748b642a2577bb96c48c5c75cf921
Pranesh-962003 <pranesh962003.p@gmail.com>
2024-11-27 13:55:42 +0530

    Update EditAdmin.jsx

--- my-app/src/components/Admin/EditAdmin.jsx
+++ my-app/src/components/Admin/EditAdmin.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
-import toast from 'react-hot-toast';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Employee/CSS/UpdateEmployee.css'; // Import custom CSS file
 
